64d777b99d513d6664457c725141deaf162216dd
AzeemJaved7 <163923406+AzeemJaved7@users.noreply.github.com>
2024-03-21 06:12:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+let FavNumber = "22";
+let message = `My favourite number is ${FavNumber}`;
+console.log(message);
+let PNumber = '94168';
+console.log(PNumber);
---
+++ app.ts
@@ -0,0 +1,8 @@
+let FavNumber: string= "22";
+let message: string= `My favourite number is ${FavNumber}`;
+console.log(message)
+let PNumber: string= '94168';
+console.log(PNumber)
+
+
+
